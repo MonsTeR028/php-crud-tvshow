c73c2d5d5549c3a99f0a9043869014ff4690ad75
audi0010 <tristan.audinot@etudiant.univ-reims.fr>
2024-06-10 11:42:04 +0200

    Ajout de la méthode de classe findAll de la classe TVShowCollection

--- src/Entity/Collection/TVShowCollection.php
+++ src/Entity/Collection/TVShowCollection.php
@@ -2,7 +2,25 @@
 
 namespace Entity\Collection;
 
+use Database\MyPdo;
+use Entity\Album;
+use Entity\TVShow;
+
 class TVShowCollection
 {
-
+    /**
+     * @return TVShow[]
+     */
+    public static function findAll() : array
+    {
+        $requete = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+                SELECT *
+                FROM tvshow
+                ORDER BY name;
+            SQL
+        );
+        $requete->execute();
+        return $requete->fetchAll(\PDO::FETCH_CLASS, TVShow::class);
+    }
 }
