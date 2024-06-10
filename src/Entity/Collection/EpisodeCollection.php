ce9d9bf20ab629eef2ace294434417b2f200b5e5
gene0024 <gatien.genevois@etudiant.univ-reims.fr>
2024-06-10 21:42:25 +0200

    Ajout de la classe EpisodeCollection

--- src/Entity/Collection/EpisodeCollection.php
+++ src/Entity/Collection/EpisodeCollection.php
@@ -2,6 +2,26 @@
 
 namespace Entity\Collection;
 
-class EpisodeCollection {
+use Database\MyPdo;
+use Entity\Episode;
 
+class EpisodeCollection
+{
+    /**
+     * Permets de trouver les épisodes de la saison d'une série en fonction de l'identifiant de cette série
+     * @param int $seasonId : l'identifiant de la série dont on veut trouver les épisodes
+     * @return array|false : tableau des épisodes de la série
+     */
+    public function findBySeasonId(int $seasonId): false|array
+    {
+        $episodeStmt = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+            SELECT *
+            FROM episode
+            WHERE seasonId =: seasonId
+            SQL
+        );
+        $episodeStmt->execute(['seasonId' => $seasonId]);
+        return $episodeStmt->fetchAll(\PDO::FETCH_CLASS, Episode::class);
+    }
 }
