fba8bef9d3d41c1933347e1d0bcd7a70108b4c4e
audi0010 <tristan.audinot@etudiant.univ-reims.fr>
2024-06-10 19:45:44 +0200

    Ajout de méthode de classe findByTVShowId dans la classe SeasonCollection

--- src/Entity/Collection/SeasonCollection.php
+++ src/Entity/Collection/SeasonCollection.php
@@ -7,5 +7,21 @@ use Entity\Season;
 
 class SeasonCollection
 {
-
+    /**
+     * @param int $id
+     * @return Season[]
+     */
+    public static function findByTVShowId(int $id): array
+    {
+        $requete = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+                SELECT *
+                FROM season
+                WHERE tvShowId = :tvShowId
+                ORDER BY seasonNumber
+            SQL
+        );
+        $requete->execute(['tvShowId' => $id]);
+        return $requete->fetchAll(\PDO::FETCH_CLASS, Season::class);
+    }
 }
