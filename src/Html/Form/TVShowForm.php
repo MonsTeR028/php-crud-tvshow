58ae07300391a2f2601c3b5abdcd660b94755f84
gene0024 <gatien.genevois@etudiant.univ-reims.fr>
2024-06-11 14:26:52 +0200

    Correction d'une erreur de syntaxe

--- src/Html/Form/TVShowForm.php
+++ src/Html/Form/TVShowForm.php
@@ -58,10 +58,10 @@ class TVShowForm
             $id = (int) $_POST['id'];
         }
         if (!empty($_POST['name']) && !empty($_POST['originalName']) && !empty($_POST['homepage']) && !empty($_POST['overview'])) {
-            $name = $this->escapeString($this->stripTagsAndTrim($_POST['name']));
-            $originalName = $this->escapeString($this->stripTagsAndTrim($_POST['originalName']));
+            $name = $this->stripTagsAndTrim($_POST['name']);
+            $originalName = $this->stripTagsAndTrim($_POST['originalName']);
             $homepage = $_POST['homepage'];
-            $overview = $this->escapeString($this->stripTagsAndTrim($_POST['overview']));
+            $overview = $this->stripTagsAndTrim($_POST['overview']);
         } else {
             throw new ParameterException("TVShowForm - setEntityFromQueryString : Paramètre(s) invalide(s)");
         }
