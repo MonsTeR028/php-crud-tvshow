fc97d4495c7e8b5396ad508fc89e394baaaac2e2
gene0024 <gatien.genevois@etudiant.univ-reims.fr>
2024-06-11 09:47:29 +0200

    Réglage de Poster

--- public/poster.php
+++ public/poster.php
@@ -8,18 +8,15 @@ use Entity\Exception\ParameterException;
 
 try {
     if(empty($_GET['posterId']) || !is_numeric($_GET['posterId'])) {
-        http_response_code(400);
-        exit();
+        throw new ParameterException("Paramètre invalide");
     }
     $poster = Poster::findById((int) $_GET['posterId']);
     header('Content-Type: image/jpeg');
     echo $poster->getJpeg();
 
-} catch (ParameterException) {
-    http_response_code(400);
-} catch (EntityNotFoundException) {
+} catch (ParameterException|EntityNotFoundException) {
     header('Content-Type: image/jpeg');
-    echo "img/default.png";
+    readfile("img/default.png");
 } catch (Exception) {
     http_response_code(500);
 }
