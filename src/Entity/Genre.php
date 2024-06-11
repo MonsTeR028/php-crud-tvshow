0d8c8a7f3ed87a5e13b0d65ec5a1cc8efdf5161b
audi0010 <tristan.audinot@etudiant.univ-reims.fr>
2024-06-11 08:34:39 +0200

    Création de la méthode de classe findById pour la classe Genre

--- src/Entity/Genre.php
+++ src/Entity/Genre.php
@@ -2,6 +2,9 @@
 
 namespace Entity;
 
+use Database\MyPdo;
+use Entity\Exception\EntityNotFoundException;
+
 class Genre
 {
     private int $id;
@@ -14,5 +17,26 @@ class Genre
     public function getName(): string
     {
         return $this->name;
+    }
+
+    /**
+     * @throws EntityNotFoundException
+     */
+    public static function findById(int $id): Genre
+    {
+        $requete = MyPdo::getInstance()->prepare(
+            <<<'SQL'
+                SELECT *
+                FROM genre
+                WHERE id = :id
+                ORDER BY name
+            SQL
+        );
+        $requete->execute(['id' => $id]);
+        $resultat = $requete->fetchObject(Genre::class);
+        if ($resultat == null) {
+            throw new EntityNotFoundException("Genre - Le genre (id: {$id}) n'existe pas");
+        }
+        return $resultat;
     }
 }
